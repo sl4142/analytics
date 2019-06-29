26fcae80e57921cdd9d1db8844405df08ae23805
Shuo Li <sl4142@nyu.edu>
2019-06-29 09:15:01 -0400

    update common function

--- commonFunctions.py
+++ commonFunctions.py
@@ -82,15 +82,12 @@ def get_symbols_multiple_pages(url, tag="a", attr="", fromText=False):
             total_results = cur.get_text().split(" ")[2]
             break
     total_urls = int(int(total_results)/100)
-    urls = []
+    symbols = []
     for i in range(total_urls+1):
-        urls.append(url + "?count=100&offset=" + str(i*100))
-    if not urls:
+        cur_url = url + "?count=100&offset=" + str(i*100)
+        symbols += get_symbols_single_page(cur_url, tag, attr, fromText)
+    if not symbols:
         return get_symbols_single_page(url, tag, attr, fromText)
-    else:
-        symbols = []
-        for cur_url in urls:
-            symbols += get_symbols_single_page(cur_url, tag, attr, fromText)
     print("Get Symbol Finished")
     return symbols
     
